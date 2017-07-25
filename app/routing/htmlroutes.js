ee6f1cc30032c2fee60de71b7c271d4d84952da1
Revinate <revinate@calvisitor-10-105-170-168.calvisitor.1918.berkeley.edu>
2017-07-25 14:48:07 -0700

    fixing bugs

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -5,11 +5,11 @@ module.exports = function(app) {
 
   // HTML GET Request
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
