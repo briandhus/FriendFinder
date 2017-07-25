13152954e2108c918e7d5889ed4f0df31d29a369
Revinate <revinate@Revinates-MacBook-Air.local>
2017-07-24 22:48:48 -0700

    added images, submit button on survey, fixed routes

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+  {
+    name: 'Kermit The Frog',
+    photo: '../../images/kermit.jpg',
+    scores: [3, 3, 5, 5, 5, 1, 1, 5, 5, 4],
+    totalScore: 37
+  },
+  {
+    name: 'Miss Piggy',
+    photo: '../../images/piggy.jpg',
+    scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 1],
+    totalScore: 28
+  },
+  {
+    name: 'Animal',
+    photo: '../../images/animal.jpg',
+    scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 2],
+    totalScore: 29
+  },
+  {
+    name: 'Fozzie Bear',
+    photo: '../../images/fozzie.jpg',
+    scores: [3, 3, 5, 4, 5, 1, 1, 5, 5, 4],
+    totalScore: 36
+  }
+];
+module.exports = friends;
---
+++ app/routing/apiroutes.js
@@ -0,0 +1,44 @@
+var path = require("path");
+var friends = require('../data/friends.js')
+
+module.exports = function(app) {
+  
+
+  app.get("/api/friends", function(req, res) {
+    // res.sendFile(path.join(__dirname, "/../data/friends.js"));
+    res.json(friends)
+  });
+
+  // post route
+  app.post('/api/friends', function(req, res) {
+    var newFriend = req.body;
+    console.log('new', newFriend);
+    var total = 0;
+    for (var i = 0; i < req.body.scores.length; i++) {
+      total += parseInt(req.body.scores[i]);
+    }
+    var greatestDifference = 1000;
+    var bestFriend = "";
+    newFriend.totalScore = total;
+    for (var i = 0; i < friends.length; i++) {
+      var friendA = 0;
+      var friendB = 0;
+      if (newFriend.totalScore < friends[i].totalScore) {
+        friendA = friends[i].totalScore;
+        friendB = newFriend.totalScore;
+      } else {
+        friendA = newFriend.totalScore;
+        friendB = friends[i].totalScore;
+      }
+      var difference = newFriend.totalScore - friends[i].totalScore;
+      if (difference < greatestDifference) {
+        greatestDifference = difference;
+        bestFriend = friends[i].name;
+      }
+    }
+    friends.push(newFriend);
+    res.json({name: bestFriend});
+  });
+};
+
+// comparisons on this page
